459a72925dcc042d51d1c2aca4c9271cddf74358
aakashDeveloper <ahanda205@gmail.com>
2020-06-04 19:38:59 +0530

    a

---
+++ first.js
@@ -0,0 +1,140 @@
+Es 
+Ecma-Script
+Es5
+Es6
+Es7
+------------------------
+Es5
+------------------------
+DataType
+Total 5 DataType
+
+String = '4444',"44432","3r3derf", 'fefeg', `knjndj`, '345.35'
+Number = 454,3, 464.4646,464564563456
+Boolean= true, false
+
+null
+undefined
+///////////Declare variable//////////
+var a = 10
+var b = "hiii"
+var c = true
+undefined
+typeof(a)
+"number"
+typeof(b)
+"string"
+typeof(c)
+"boolean"
+
+////////////Operation//////////
+var a = 10
+var b = 20
+undefined
+a+b
+30
+a-b
+-10
+a*b
+200
+a/b
+0.5
+
+4%2
+0
+3%3
+0
+3%2
+1
+2%2
+0
+
+2%4
+2
+3%4
+3
+4%4
+0
+Reminder for smaller to higher is always smaller
+
+var a = "hi "
+undefined
+var b = "JavaScript"
+undefined
+a+b
+"hi JavaScript"
+a-b
+NaN(Not a number)
+a*b
+NaN
+a/b
+NaN
+
+var a = "hi "
+undefined
+var b = 10
+undefined
+a+b
+"hi 10"
+var c = 20
+undefined
+b+c
+30
+a+c
+"hi 20"
+c+a
+"20hi "
+a-b
+NaN
+a-c
+NaN
+
+string+string = string
+string+number = string
+number+string = string
+number+number=number
+
+10+'10'+10
+"101010"
+10+10+'10'
+"2010"
+'10'+10+10
+"101010"
+
+
+10+'10'+10-1
+101009
+10+10+'10'-1
+2009
+'10'+10+10-1
+101009
+
+10+'10'+10
+"101010"
+"101010"-1
+101009
+
+
+'a'-1
+NaN
+'10'-'1'
+9
+'10a'-1
+NaN
+'10'*'2'
+20
+'10'*2
+20
+'10'+'1'
+"101"
+'10'-'1'
+9
+'10'-'1a'
+NaN
+
+var a ='10'
+undefined
+typeof(a)
+"string"
+'10'-'1'
+9
