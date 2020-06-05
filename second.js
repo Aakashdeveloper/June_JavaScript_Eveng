979b1c89502bc6b85dffd0b3e520fec57d5ae42c
aakashDeveloper <ahanda205@gmail.com>
2020-06-05 19:36:10 +0530

    a

---
+++ second.js
@@ -0,0 +1,153 @@
+true is equal to 1
+false is equal to 0
+
+true+true = 2
+true+false = 1
+false+true = 1
+false+false =0
+
+true+true
+2
+true+false
+1
+false+false
+0
+
+true-true*5
+-4
+(true-true)*5
+0
+true+true
+2
+"hi"+true
+"hitrue"
+"hi"-true
+NaN
+
+"1"-"5"
+-4
+"true"-"true"
+
+//////////////////////////
+=  assignment
+== compare the data
+=== compare the data & dataType
+
+var a = 10
+
+a == 10
+
+var a = 10
+undefined
+a
+10
+a == 10
+true
+a == '10'
+true
+a === '10'
+false
+typeof(a)
+"number"
+var b = '10'
+undefined
+typeof(b)
+"string"
+a == b
+true
+a===b
+false
+
+
+var a = "Hi"
+undefined
+var b = "Hi"
+undefined
+var c = "hi"
+undefined
+a==b
+true
+a==c
+false
+a===b
+true
+a===c
+false
+
+////////////////////////////////
+
+var a = 10
+var b = 11
+
+a>b
+
+
+var a = 10
+var b = 11
+
+a>b
+false
+a<b
+true
+var c = 10
+undefined
+a >= b
+false
+a >= c
+true
+a >= c
+
+a !== b
+true
+a != b
+true
+
+
+a >= b
+
+
+//////Nagation/////
+var a = true
+undefined
+!a
+false
+var b = !a
+undefined
+b
+false
+!b
+true
+
+var a = 0
+!a
+
+var a = 0
+undefined
+!a
+true
+ var a = 1
+undefined
+!a
+false
+var c = 10
+undefined
+!c
+false
+var c = 40
+undefined
+!c
+false
+
+///////////////////
+Truthy 0> true
+Falsy  0 false null undefined
+
+
+var a = "hi"
+undefined
+!a
+false
+var a = null
+undefined
+!a
+true
---
+++ thrid.js
@@ -0,0 +1,41 @@
+/////String Operations////
+var a = "aakash"
+undefined
+a.toUpperCase()
+"AAKASH"
+a.toLowerCase()
+"aakash"
+
+var a = "aakash"
+"Aakash"
+
+
+var a = "Hi"
+var b = "Hi"
+var c = "hi"
+
+
+a.toLowerCase() == c.toLowerCase()
+
+var c = "hi"
+undefined
+a
+"Hi"
+c
+"hi"
+a == c
+false
+a.toLowerCase() == c.toLowerCase()
+true
+
+var a = "Hi"
+var b = "Hi"
+var c = "hi"
+
+undefined
+a == b
+true
+a == c
+false
+a.toLowerCase() == c.toLowerCase()
+true
