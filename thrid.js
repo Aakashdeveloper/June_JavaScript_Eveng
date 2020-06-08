9864f94413888ea7003dbf777876ea9af7a69063
aakashDeveloper <ahanda205@gmail.com>
2020-06-08 19:49:49 +0530

    a

--- thrid.js
+++ thrid.js
@@ -39,3 +39,124 @@ a == c
 false
 a.toLowerCase() == c.toLowerCase()
 true
+
+var name = "john"
+undefined
+name.length
+4
+var name = "   John  "
+undefined
+name.length
+9
+name.trim()
+"John"
+var a = "Brain      james"
+undefined
+a.trim()
+"Brain      james"
+
+Slice 
+var city="Amsterdam"
+
+var city="Amsterdam"
+undefined
+city.slice(1)
+"msterdam"
+city.slice(2)
+"sterdam"
+city.slice(1,2)
+"m"
+city[0]
+"A"
+city.slice(2,4)
+"ste"
+
+var city="Amsterdam"
+undefined
+city.charAt(0)
+"A"
+city.charAt(3)
+"t"
+
+var city = "pAriS"
+
+city.charAt(0)
+
+var city = "pAriS"
+
+city.charAt(0)
+"p"
+city.charAt(0).toUpperCase()
+"P"
+city.slice(1)
+"AriS"
+city.slice(1).toLowerCase()
+"aris"
+city.charAt(0).toUpperCase()+city.slice(1).toLowerCase()
+"Paris"
+
+city.charAt(0).toUpperCase()+city.toLowerCase()
+
+var greet = "Welcome to JavaScript"
+undefined
+greet.replace('JavaScript','JS')
+"Welcome to JS"
+var greet = "JavaScript Welcome to JavaScript"
+undefined
+greet.replace('JavaScript','JS')
+"JS Welcome to JavaScript"
+greet.replace('JavaScript','JS').replace('JavaScript','JS')
+"JS Welcome to JS"
+greet.replace(/JavaScript/,'JS')
+"JS Welcome to JavaScript"
+greet.replace(/JavaScript/g,'JS')
+"JS Welcome to JS"
+var subject = "JavaScript"
+undefined
+subject.replace('a','A')
+"JAvaScript"
+subject.replace(/a/g,'A')
+"JAvAScript"
+var greet = "Brian    James"
+undefined
+greet.replace(/ /g,'-')
+"Brian----James"
+greet.replace(/   /g,'-')
+"Brian- James"
+greet.replace(/    /g,'')
+"BrianJames"
+greet.replace(/    /g,' ')
+"Brian James"
+
+
+var city="London"
+undefined
+city.charAt(2)
+"n"
+city.indexOf('d')
+3
+city.indexOf('z')
+-1
+city.indexOf('j')
+-1
+city.indexOf('n')
+2
+
+slice(start,end)
+substring(start,end)
+substr(start,length)
+
+var a = "Hongkong"
+undefined
+a.slice(1)
+"ongkong"
+a.substring(1)
+"ongkong"
+a.substr(1)
+"ongkong"
+a.slice(1,3) > (1,2)
+"on"
+a.substring(1,3) > (1,2)
+"on"
+a.substr(1,3) > (1,2,3)
+"ong"
